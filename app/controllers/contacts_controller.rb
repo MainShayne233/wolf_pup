46975f57f3ef99e99f923b0717b9a41d47318d4b
MainShayne233 <shaynetremblay@hotmail.com>
2016-01-13 21:38:47 -0500

    plz work

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,6 @@ class ContactsController < ApplicationController
 		@contact.request = request
 		if @contact.deliver
 			flash.now[:error] = nil
-			redirect_to root_path
 		else
 			flash.now[:error] = 'Cannot send message.'
 			render :new
